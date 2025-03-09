66d40135cb343e3515d69925b592353e7aabb2a9
RyotaKimura <kimuchan173049@gmail.com>
2025-03-10 01:38:57 +0900

    path修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,63 @@
+# # アプリケーションサーバの性能を決定する
+# worker_processes 2
+
+# # アプリケーションの設置されているディレクトリを指定
+# working_directory "/src"
+
+# # ポート番号を指定
+# listen 3000, tcp_nopush: true
+
+# # プロセスIDの保存先を指定
+# pid "/src/tmp/pids/unicorn.pid"
+
+# # エラーのログを記録するファイルを指定
+# stderr_path "/src/log/unicorn.stderr.log"
+
+# # 通常のログを記録するファイルを指定
+# stdout_path "/src/log/unicorn.stdout.log"
+
+# #応答時間を待つ上限時間を設定
+# timeout 30
+
+# # ダウンタイムなしでUnicornを再起動時する
+# preload_app true
+
+# before_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+#     Process.kill 'QUIT', Process.pid
+#   end
+
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.connection.disconnect!
+# end
+
+# after_fork do |server, worker|
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.establish_connection
+# end
+
+
+# rootパスのディレクトリを指定
+root_path = File.expand_path('./src', __FILE__)
+
 # アプリケーションサーバの性能を決定する
 worker_processes 2
 
 # アプリケーションの設置されているディレクトリを指定
-working_directory "/src"
-
-# ポート番号を指定
-listen 3000, tcp_nopush: true
+working_directory root_path
 
 # プロセスIDの保存先を指定
-pid "/src/tmp/pids/unicorn.pid"
+pid "#{root_path}/tmp/pids/unicorn.pid"
+
+# ポート番号を指定
+listen "#{root_path}/tmp/sockets/unicorn.sock"
 
 # エラーのログを記録するファイルを指定
-stderr_path "/src/log/unicorn.stderr.log"
+stderr_path "#{root_path}/log/unicorn.stderr.log"
 
 # 通常のログを記録するファイルを指定
-stdout_path "/src/log/unicorn.stdout.log"
+stdout_path "#{root_path}/log/unicorn.stdout.log"
 
 #応答時間を待つ上限時間を設定
 timeout 30
@@ -22,17 +65,31 @@ timeout 30
 # ダウンタイムなしでUnicornを再起動時する
 preload_app true
 
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+run_once = true
+
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false # prevent from firing again
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
+    end
+  end
 end
 
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
