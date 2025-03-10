fd946f02c80d971dbeafa3ec1085cedc805d882a
RyotaKimura <kimuchan173049@gmail.com>
2025-03-11 01:09:41 +0900

    ドメイン設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass　# Laravel Mixを使用する場合、不必要
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false # true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -93,9 +93,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # 追加：devise
-  # config.action_mailer.default_url_options = { host: 'https://46.51.253.104/' }
-  # config.action_mailer.default_url_options = { host: 'https://oshitetsu.com/' }
-  config.action_mailer.default_url_options = { host: 'http://35.74.213.11:3000/' }
+  # config.action_mailer.default_url_options = { host: 'http://35.74.213.11:3000/' }
+  config.action_mailer.default_url_options = { host: 'http://oshitetsu.com/' }
+
 
   # 追加：Gmailの設定
   config.action_mailer.raise_delivery_errors = true # メール送信時にエラーが発生した場合、エラーを表示
