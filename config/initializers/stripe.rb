35ed5c300d2ed3f3f05a67213862ca4f176f498a
RyotaKimura <kimuchan173049@gmail.com>
2025-04-05 10:57:19 +0900

    API Version更新

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 require 'stripe'
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 # Stripe.api_version = '2024-09-30.acacia'
-Stripe.api_version = '2023-08-16'
+Stripe.api_version = ENV['STRIPE_API_VERSION']
