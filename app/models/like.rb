5db11e108d8dbf0f0b62148274ab9a34e319e488
RyotaKimura <kimuchan173049@gmail.com>
2025-04-07 23:55:56 +0900

    いいねの集計方法を修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -10,15 +10,17 @@ class Like < ApplicationRecord
   # 1週間以内のいいねを user_id ごとに集計する関数
   def self.recent_likes_count_weekly(profiles, start_of_last_week, end_of_last_week)
     recent_likes_count_weekly = self
-      .where(created_at: start_of_last_week..end_of_last_week)  # 先週のデータを取得
-      .group(:user_id)                                          # user_id 毎にグループ化
-      .order('COUNT(user_id) DESC')                             # いいねの数が多い順に並べる
-      .limit(10)                                                # 上位10のみ
-      .count                                                    # 各 user_id 毎のいいね数を集計
-    
-    recent_likes_count_weekly = recent_likes_count_weekly.map do |user_id, count|
-      profile = profiles.find_by(user_id: user_id)  # ユーザーを検索
-      [profile&.nickname || 'Unknown User', count, user_id]  # ニックネームといいね数とuser_idのペア
+      .where(created_at: start_of_last_week..end_of_last_week)        # 先週のデータを取得
+      .joins("INNER JOIN posts ON posts.id = #{table_name}.post_id")  # 「いいね」と「その対象となった投稿」をセットで扱う
+      .group("posts.user_id")                                         # 投稿者ごとにグループ化
+      .order("COUNT(posts.user_id) DESC")                             # いいねの数が多い順に並べる
+      .limit(10)                                                      # 上位10のみ
+      .count                                                          # 投稿者ごとの「いいね数」をカウント
+
+    # 結果を整形
+    recent_likes_count_weekly.map do |post_user_id, count|
+      profile = profiles.find_by(user_id: post_user_id)
+      [profile&.nickname || 'Unknown User', count, post_user_id]
     end
     return recent_likes_count_weekly
   end
