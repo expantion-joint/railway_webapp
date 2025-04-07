5c840b823d4e26f05bec06c70015bf26415fabdd
RyotaKimura <kimuchan173049@gmail.com>
2025-04-08 01:10:50 +0900

    集計期間の修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -17,7 +17,7 @@ class LikesController < ApplicationController
 
   def show
     # 先週の開始日と終了日を計算
-    @start_of_last_week = 2.week.ago.beginning_of_week
+    @start_of_last_week = 1.week.ago.beginning_of_week
     @end_of_last_week = 1.week.ago.end_of_week
     # 1週間以内のいいねを user_id ごとに集計
     @recent_likes_count_weekly = Like.recent_likes_count_weekly(Profile.all, @start_of_last_week, @end_of_last_week)
